f1549a4fbab33dc1af9fba9f14d68a14a96586b9
Grant <ehgrant@usgs.gov>
2025-03-14 16:08:46 -0400

    new code to conduct survival analysis

--- code/TickConsumption.R
+++ code/TickConsumption.R
@@ -1,43 +1,74 @@
+## copy of TickConsumption.R - test area for survival model
 ## analysis of experimental data for invertebrate feeding trials to red-backed 
 ## salamanders, in simple and complex mesocosms
-## The purpose of this script is to _____________ 
+## The purpose of this script is to calculate and compare survival curves for 
+## prey exposure experiment
 ## Written by ehgrant
 ## Questions: ehgrant at usgs dot gov
+####################
+#0. Preliminaries 
+# load libraries
+####################
+library(survival)
+library(coxme)#for mixed effects models
+library(ggsurvfit)
+library(gtsummary) #use to generate table of cox Hazard Ratio results
+library(riskRegression) #use to plot 
+library(adjustedCurves) #use to plot 
+library(rms) #needed in adjustedCurves
+library(pammtools) #needed in adjustedCurves
+library(dplyr)
+
 #########################
 #1. import data
 #########################
+# Note the format of the data from experimental trials.
+# For each salamander mesocosm, reformat the daily count data to 
+# create individual prey survival time datatable.
+prey<-read.csv(file = "C:/Users/ehgrant/OneDrive - DOI/Documents/GIThub/Ticks/Data/reformattedtrial3.csv",header=TRUE)
+#rename sal -> cluster  for use in riskRegression package
+prey<-rename(prey,cluster = sal)
+prey$Rx<-as.factor(prey$Rx) #specify as factor
+########################
+#2. survival analysis
+########################
 
-# preliminaries #
-# load libraries, read data, setup matrices for output, specify priors #
-#load library#
-library(dplyr)
-#read data
-prey<-read.csv(file = "../Data/tickFeedingRd1.csv",header=TRUE)
+###Kaplan-Meier survival model
+#overall survival analysis (individual salamanders as a factor)
+(s1 <- survfit(Surv(Eaten_day,outcome) ~ Rx+cluster, data = prey))
 
+#plot the survival curves
+survfit2(Surv(Eaten_day,outcome) ~ Rx+cluster, data = prey) %>% 
+  ggsurvfit() +
+  labs(
+    x = "Days",
+    y = "Overall survival probability"
+  ) 
+
+### Cox proportional hazards model 
+#mixed effects cox ph model (random intercept for each salamander)
+(coxmod.me<-coxme(Surv(Eaten_day,outcome) ~ Rx + (1|cluster), data = prey))
+
+#same as above but using frailty model form
+(coxmod.frail<-coxph(Surv(Eaten_day,outcome) ~ Rx + frailty(cluster), data = prey))
+summary(coxmod.frail) #exp(coef) is the Hazard Ratio for a covariate
 #########################
-#2. create summaries 
+#3. Plot survival curve by treatment
 #########################
-#calculate number eaten each day
-#('eaten')
+predict_fun <- function(...) {
+  1 - predictRisk(...)
+}
 
-#Calculate mean daily consumption by individual#
-feedRate<-prey %>%
-  group_by(sal,Habitat_Type) %>%
-  summarise_at(vars(eaten), mean)
+# bootstrap confidence intervals using adjustedCurves package
+adjsurv.sals.bs <- adjustedsurv(data=prey,
+                                variable="Rx",
+                                ev_time="Eaten_day",
+                                event="outcome",
+                                method="direct",
+                                bootstrap=TRUE,
+                                n_boot=500,
+                                outcome_model=coxmod.frail,
+                                predict_fun=predict_fun)
+#now show the plot
+plot(adjsurv.sals.bs, conf_int=TRUE, use_boot=TRUE)
 
-#Calculate overall average and SD by Rx#
-RxMean<-feedRate %>%
-  group_by(Habitat_Type) %>%
-  summarise(mean=mean(eaten), sd=sd(eaten))
-#########################
-#3. plot
-#########################
-
-
-
-#########################
-#4. test for difference
-#########################
-
-owdiff <- aov(eaten ~ Habitat_Type, data = feedRate)
-summary(owdiff)
