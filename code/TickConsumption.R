9c2f65ae602362213289642877db9bb84015cd82
Grant <ehgrant@usgs.gov>
2025-03-14 16:12:45 -0400

    small

--- code/TickConsumption.R
+++ code/TickConsumption.R
@@ -25,7 +25,7 @@ library(dplyr)
 # Note the format of the data from experimental trials.
 # For each salamander mesocosm, reformat the daily count data to 
 # create individual prey survival time datatable.
-prey<-read.csv(file = "C:/Users/ehgrant/OneDrive - DOI/Documents/GIThub/Ticks/Data/reformattedtrial3.csv",header=TRUE)
+prey<-read.csv(file = "data/reformattedtrial3.csv",header=TRUE)
 #rename sal -> cluster  for use in riskRegression package
 prey<-rename(prey,cluster = sal)
 prey$Rx<-as.factor(prey$Rx) #specify as factor
